5160b5d4a017c143f703ee1ca835ba1ecf8c8cb5
nivek29 <kevinmezamc@gmail.com>
2021-01-27 21:51:38 +0000

    Final

--- src/main.js
+++ src/main.js
@@ -7,4 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+  generarValor();
+  generarPalo();
 };
+
+function generarValor() {
+  let valor = Math.floor(Math.random() * (14 - 0) + 0);
+  let valores = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  var valorCarta = document.getElementById("number");
+  valorCarta.innerHTML = valores[valor];
+  console.log(valores[valor]);
+}
+
+function generarPalo() {
+  let valor = Math.floor(Math.random() * (4 - 0) + 0);
+  let palos = ["card-diamond", "card-spade", "card-club", "card-hearth"];
+  var elemento = document.getElementsByTagName("h2");
+  for (var i = 0; i < elemento.length; i++) {
+    elemento[i].className = palos[valor];
+  }
+}
