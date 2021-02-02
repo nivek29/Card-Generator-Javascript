bb16f69d0efb42a21290972c587d08c39b5df9cf
nivek29 <kevinmezamc@gmail.com>
2021-02-02 12:23:08 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,15 +6,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
-  generarValor();
-  generarPalo();
+  
 };
 
-function generarValor() {
-  let valor = Math.floor(Math.random() * (14 - 0) + 0);
-  let valores = [
-    "1",
+function arrayNum(cantidad) {
+  var arr = [];
+
+  while (0 < cantidad) {
+    arr.push(Math.floor(Math.random() * (13 - 0) + 0));
+    --cantidad;
+  }
+  return arr;
+}
+
+function ordenarArreglo(array) {
+  let wall = array.length - 1;
+  while (wall > 0) {
+    let index = 0;
+    while (index < wall) {
+      if (array[index] > array[index + 1]) {
+        let aux = array[index];
+        array[index] = array[index + 1];
+        array[index + 1] = aux;
+      }
+      index++;
+    }
+    wall--;
+  }
+  return array;
+}
+
+function generarValor(arrayOrdenado) {
+  let valoresArray = [];
+  let x = 0;
+  let valoresPred = [
+    "A",
     "2",
     "3",
     "4",
@@ -26,20 +52,37 @@ function generarValor() {
     "10",
     "J",
     "Q",
-    "K",
-    "A"
+    "K"
   ];
-
-  var valorCarta = document.getElementsByTagName("p");
-  valorCarta[0].innerHTML = valores[valor];
-  valorCarta[1].innerHTML = valores[valor];
-  console.log(valores[valor]);
+  while (x < arrayOrdenado.length) {
+    valoresArray[x] = valoresPred[arrayOrdenado[x]];
+  }
+  return valoresArray;
 }
+
 function generarPalo() {
   let valor = Math.floor(Math.random() * (4 - 0) + 0);
   let palos = ["card-diamond", "card-spade", "card-club", "card-hearth"];
-  var elemento = document.getElementsByTagName("h2");
-  for (var i = 0; i < elemento.length; i++) {
-    elemento[i].className = palos[valor];
+  return palos[valor];
+}
+
+function plantillaCarta(sim, valorNum) {
+  let totalCartas = 0;
+  while (totalCartas < valorNum.length) {
+    var cartaContent = document.createElement("div");
+    cartaContent.innerHTML =
+      "<div class=card style=width: 3rem;> <div><h2 class='" +
+      sim +
+      "'></h2></div><div><p class= text-center id= number>'" +
+      valorNum +
+      "'</p></div><div class=text-right><h2 class='" +
+      sim +
+      "'></h2></div></div>";
+
+    document.querySelector("div").appendChild(cartaContent);
+    cartaContent.classList.add("col-lg-2");
+    cartaContent.classList.add("col-md-3");
+    cartaContent.classList.add("col-sm-4");
+    cartaContent.classList.add("bg-info");
   }
 }
